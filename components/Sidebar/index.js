2f529180714f787f3e3c3ce5d708b61e0b981cb6
mohit mandal <25281526+mohitmandal40@users.noreply.github.com>
2023-07-14 10:59:55 +0530

    update css typo

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useContext, useState } from "react";
-import classes from "./Sidebar.module.css";
+import classes from "./sidebar.module.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
